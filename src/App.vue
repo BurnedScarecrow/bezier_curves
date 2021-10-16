b8659619e2ad1e673854cdd69f8e7c67f5363e6d
Alexander Evsikov <evsik.sasha@gmail.com>
2021-10-16 22:13:43 +0300

    Main functional added

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Form @draw="getCoords" />
+    <Canvas ref="canvas" :data="coords" />
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Canvas from "./components/Canvas.vue";
+import Form from "./components/Form.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      coords: Array(),
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    Form,
+    Canvas,
+  },
+  methods: {
+    getCoords(data) {
+      this.coords = data;
+      this.$refs.canvas.redraw();
+    },
+  },
+};
 </script>
 
 <style>
+body,
+html {
+  margin: 0;
+  background: #07afda;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Courier New", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+.container {
+  display: flex;
 }
 </style>
